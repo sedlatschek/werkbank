70fe5d883b2fc8968ea0f1a04c78628e61750cbe
Simon Sedlatschek <s.sedlatschek@telekom.de>
2021-07-05 23:53:27 +0200

    add build workflow

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,12 @@ module.exports = {
         win: {
           icon: 'public/win.ico',
         },
+        mac: {
+          icon: 'public/icon-1536x1536.png',
+        },
+        linux: {
+          icon: 'src/assets/icon',
+        },
       },
       nodeIntegration: true,
     },
